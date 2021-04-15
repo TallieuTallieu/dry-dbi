228e09b68e8a77bbb19f28dc216908a7aa48fe5f
Nathan Geerinck <nathan@tnt.be>
2021-04-15 09:38:50 +0200

    Added orderBy function to the BaseRepository

--- src/BaseRepository.php
+++ src/BaseRepository.php
@@ -4,12 +4,20 @@ namespace Tnt\Dbi;
 
 use Tnt\Dbi\Contracts\RepositoryInterface;
 use Tnt\Dbi\Criteria\LimitOffset;
+use Tnt\Dbi\Criteria\OrderBy;
 
 class BaseRepository extends Repository implements RepositoryInterface
 {
     public function amount($amount = 30, $offset = 0)
     {
         $this->addCriteria(new LimitOffset($amount, $offset));
+
+        return $this;
+    }
+
+    public function orderBy($column, $order = 'ASC')
+    {
+        $this->addCriteria(new OrderBy($column, $order));
 
         return $this;
     }
